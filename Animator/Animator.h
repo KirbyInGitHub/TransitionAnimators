f251fd8ea44863f37d03707e37dde8975d62e6c9
zhangpeng <zhangpeng@ezbuy.com>
2017-07-02 22:35:00 +0800

    use framework

---
+++ Animator/Animator.h
@@ -0,0 +1,19 @@
+//
+//  Animator.h
+//  Animator
+//
+//  Created by 张鹏 on 2017/7/2.
+//  Copyright © 2017年 大白菜. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Animator.
+FOUNDATION_EXPORT double AnimatorVersionNumber;
+
+//! Project version string for Animator.
+FOUNDATION_EXPORT const unsigned char AnimatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Animator/PublicHeader.h>
+
+
--- TransitionAnimators.podspec
+++ TransitionAnimators.podspec
@@ -11,5 +11,5 @@ Pod::Spec.new do |s|
   s.tvos.deployment_target = '9.0'
   s.watchos.deployment_target = '2.0'
 
-  s.source_files = 'Animator/*.swift'
+  s.source_files = 'Animator/*.*'
 end
